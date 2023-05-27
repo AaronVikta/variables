76d989530a6824892eb2075608ef7c0d4bb7b7da
AaronVikta <aeinsteinvikta@gmail.com>
2023-05-27 22:33:20 +0100

    Loops and Control flow

--- src/main.rs
+++ src/main.rs
@@ -26,5 +26,20 @@ fn main() {
     
     let element = a[index];
 
-    println!("The value of the element at index {index} is {element}")
+    println!("The value of the element at index {index} is {element}");
+    loops();
+}
+
+fn loops(){
+    let mut counter =0;
+
+    let result = loop {
+        counter +=1;
+
+        if counter == 10{
+            break counter *2;
+        }
+    };
+
+    println!("The result is {result}");
 }
